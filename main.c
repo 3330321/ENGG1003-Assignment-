ee0768e634d17793c7c7476a90937b6450c33053
Isaac DeMaria <c3330321@uon.edu.au>
2019-04-03 07:24:44 +0000

    Commit at end of week 6 lab

--- main.c
+++ main.c
@@ -1,34 +1,31 @@
 #include <stdio.h>
 /* This is my first commit for the beginning of my week 6 workshop*/
 int main() {
-    int choice //The two choice ints are used to decide the required function.
-    int choice_two
-    printf("Press 1 for encryption or 2 for decryption\n"); //Choice for use to encrypt or decrypt
-    scanf("%d", &choice);
-    if(choice=1){
-        printf("Press 1 to encrypt a shift or 2 to encrypt a substitution");
-        scanf("%d", &choice_two);
-        if(choice_two=1) { //The use has chosen to encrypt a shift
-            printf("Please enter the text you with to encrypt");
-            scanf();
-            printf("")
-        else //The user has chosen to encrypt a substitution
-            printf("Please enter the text you wish to encrpyt");
-        }
-    else //The user has chose to decrypt
-        printf("Press 1 to decrypt a shift or 2 to decrypt a substition");
-   `    scanf("%d", &choice_two);
-        if(choice_two=1){ //The use has chosen to decrypt a shift
-            printf("Please enter the text you wish to decrypt");
-            else //The user has chosen to decrypt a substitution
-            printf("Please enter the text you wish to decrypt");
-        }
+    int choice;
+    printf("Press 1 for a substitution encrpytion, 2 for a substitution decryption, 3 for a shift encrpytion or 4 for a substitution decrpytion");
+choice == 2;
+scanf("%d", &choice);
+switch(choice) { 
+    case: 1 {
+        printf("You have chosen substitution encryption\n");
+        printf("Please enter the plain text you wish to encrypt\n");
     }
+    case: 2 {
+        printf("You have chosen substitution decrpytion\n");
+        printf("Please enter the cipher text you wish to decrypt\n");
+        printf("Please enter ") //How to do the substitution
+    }
+    case: 3 {
+        printf("You have chosen shift encrpytion\n");
+        printf("Please enter the plain text you wish to encrypt\n");
+        printf("Please enter the shift\n");
+    }
+    case: 4 {
+        printf("You have chosen shift decrpytion"\n);
+        printf("Please enter the cipher text you wish to decrypt\n");
+        printf("Please enter the shift\n");
+    }
+}
 
-   
-   
-   
-   
-   
 return 0;
 }
