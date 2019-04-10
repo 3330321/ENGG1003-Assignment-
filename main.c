47f0dafad6a5a654e3ad16a04e796f54e3fe1fdd
Isaac DeMaria <c3330321@uon.edu.au>
2019-04-10 07:35:30 +0000

    Fixed the switch case

--- main.c
+++ main.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
-/* This is my first commit for the beginning of my week 6 workshop*/
+/* This is my commit at end of workshop*/
 int main() {
     int choice;
-    printf("Press 1 for a substitution encrpytion, 2 for a substitution decryption, 3 for a shift encrpytion or 4 for a substitution decrpytion");
-choice == 2;
+    printf("Press 1 for a substitution encrpytion, 2 for a substitution decryption, 3 for a shift encrpytion or 4 for a substitution decrpytion\n");
 scanf("%d", &choice);
 switch(choice) { 
-    case: 1 {
+    case 1: 
+    {
         printf("You have chosen substitution encryption\n");
         printf("Please enter the plain text you wish to encrypt\n");
+        break;
     }
-    case: 2 {
+    case 2:
+    {
         printf("You have chosen substitution decrpytion\n");
         printf("Please enter the cipher text you wish to decrypt\n");
-        printf("Please enter ") //How to do the substitution
+        printf("Please enter "); //How to do the substitution
+        break;
     }
-    case: 3 {
+    case 3: 
+    {
         printf("You have chosen shift encrpytion\n");
         printf("Please enter the plain text you wish to encrypt\n");
         printf("Please enter the shift\n");
+        break;
     }
-    case: 4 {
-        printf("You have chosen shift decrpytion"\n);
+    case 4:
+    {
+        printf("You have chosen shift decrpytion/n");
         printf("Please enter the cipher text you wish to decrypt\n");
         printf("Please enter the shift\n");
+        break;
     }
 }
 
